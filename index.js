aa264b50e5d5407146f68add46bfe4b823a0db18
Tadeusz Woźniak <twozniak@consdata.com>
2025-02-15 22:26:56 +0100

    create home page

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
-    res.render("home.ejs")
-  });
-
+    res.render("home.ejs",{active:'home'})
+});
+app.get("/about", (req, res) => {
+    res.render("home.ejs",{active:'about'})
+});
+app.get("/services", (req, res) => {
+    res.render("home.ejs",{active:'services'})
+});
+app.get("/realizations", (req, res) => {
+    res.render("home.ejs",{active:'realizations'})
+});
+app.get("/contact", (req, res) => {
+    res.render("home.ejs",{active:'contact'})
+});
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
