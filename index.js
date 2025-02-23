065d78f5ec75dfa4b9437d4e3e1f0cb078f6a941
Tadeusz Woźniak <twozniak@consdata.com>
2025-02-23 17:23:25 +0100

    Skonczenie sekcji realizacje

--- index.js
+++ index.js
@@ -28,7 +28,15 @@ app.get("/services", (req, res) => {
     res.render("services.ejs", { active: 'services' })
 });
 app.get("/realizations", (req, res) => {
-    res.render("realizations.ejs", { active: 'realizations' })
+    const path = '/images/realizations/'
+    const projects = [
+        { image: path + "project1.jpg", title: "Nowoczesna łazienka z marmurowym wykończeniem", description: "Elegancka przestrzeń z owalnym lustrem LED, złotymi bateriami i przestronnym prysznicem" },
+        { image: path + "project2.jpg", title: "Stylowe schody drewniane", description: "Biała balustrada w klasycznym stylu z drewnianymi stopniami, idealnie komponująca się z eleganckim wnętrzem" },
+        { image: path + "project3.jpg", title: "Łazienka w stylu loftowym", description: "Połączenie drewna i industrialnych elementów, z dużym okrągłym lustrem i designerską umywalką." },
+        { image: path + "project4.jpg", title: "Podłoga w szachownicę", description: "Klasyczne czarno-białe płytki dodające wnętrzu ponadczasowego charakteru." },
+        { image: path + "project5.jpg", title: "Minimalistyczny prysznic walk-in", description: "Przejrzysta szklana ścianka i duże, białe płytki tworzą nowoczesny i przestronny design." }
+    ];
+    res.render("realizations.ejs", { active: 'realizations', projects: projects })
 });
 app.get("/contact", (req, res) => {
     res.render("contact.ejs", { active: 'contact' })
